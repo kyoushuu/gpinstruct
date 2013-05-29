f8f5f3863aa73dcdf8c3de93c655a551eddb771b
Arnel A. Borja <kyoushuu@users.sourceforge.net>
2013-05-29 10:35:51 +0800

    libgpinstruct-editor: Add instructions field to projects

--- gpinstruct-editor/gpinstruct-project-editor.c
+++ gpinstruct-editor/gpinstruct-project-editor.c
@@ -30,6 +30,7 @@ struct _GPInstructProjectEditorPrivate
 	GPInstructProject *project;
 
 	GtkWidget *title_entry;
+	GtkWidget *instructions_view;
 };
 
 #define GPINSTRUCT_PROJECT_EDITOR_GET_PRIVATE(o)  (G_TYPE_INSTANCE_GET_PRIVATE ((o), GPINSTRUCT_TYPE_PROJECT_EDITOR, GPInstructProjectEditorPrivate))
@@ -56,6 +57,23 @@ gpinstruct_project_editor_init (GPInstructProjectEditor *object)
 	gtk_widget_set_hexpand (priv->title_entry, TRUE);
 	gtk_grid_attach_next_to (GTK_GRID (object), priv->title_entry,
 	                         title_label, GTK_POS_RIGHT, 1, 1);
+
+	GtkWidget *instructions_label = gtk_label_new (_("Instructions"));
+	context = gtk_widget_get_style_context (instructions_label);
+	gtk_style_context_add_class (context, "dim-label");
+	gtk_widget_set_halign (instructions_label, GTK_ALIGN_END);
+	gtk_widget_set_valign (instructions_label, GTK_ALIGN_START);
+	gtk_container_add (GTK_CONTAINER (object), instructions_label);
+
+	GtkWidget *instructions_view_scrolled_window = gtk_scrolled_window_new (NULL, NULL);
+	gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (instructions_view_scrolled_window),
+	                                GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+	priv->instructions_view = gtk_text_view_new ();
+	gtk_container_add (GTK_CONTAINER (instructions_view_scrolled_window), priv->instructions_view);
+	gtk_text_view_set_wrap_mode (GTK_TEXT_VIEW (priv->instructions_view), GTK_WRAP_WORD_CHAR);
+	gtk_widget_set_hexpand (priv->instructions_view, TRUE);
+	gtk_grid_attach_next_to (GTK_GRID (object), instructions_view_scrolled_window,
+	                         instructions_label, GTK_POS_RIGHT, 1, 3);
 }
 
 static void
@@ -91,6 +109,23 @@ title_entry_activate (GtkEntry *entry,
 	gpinstruct_editor_window_update_tree_store (priv->window, (gpointer)priv->project);
 }
 
+static void
+instructions_buffer_changed (GtkTextBuffer *textbuffer,
+                             gpointer       user_data)
+{
+	GPInstructProjectEditor *editor = GPINSTRUCT_PROJECT_EDITOR (user_data);
+	GPInstructProjectEditorPrivate *priv = editor->priv;
+
+	GtkTextIter start, end;
+	gchar *text;
+	gtk_text_buffer_get_bounds (textbuffer, &start, &end);
+	text = gtk_text_iter_get_text (&start, &end);
+	gpinstruct_project_set_instructions (priv->project,
+	                                     text);
+	g_free (text);
+	gpinstruct_editor_window_set_modified (priv->window, TRUE);
+}
+
 
 GPInstructProjectEditor *
 gpinstruct_project_editor_new (GPInstructEditorWindow *window,
@@ -106,6 +141,12 @@ gpinstruct_project_editor_new (GPInstructEditorWindow *window,
 	                    gpinstruct_project_get_title (project));
 	g_signal_connect (priv->title_entry, "activate",
 	                  G_CALLBACK (title_entry_activate), editor);
+
+	GtkTextBuffer *buffer = gtk_text_view_get_buffer (GTK_TEXT_VIEW (priv->instructions_view));
+	gtk_text_buffer_set_text (buffer,
+	                          gpinstruct_project_get_instructions (project), -1);
+	g_signal_connect (buffer, "changed",
+	                  G_CALLBACK (instructions_buffer_changed), editor);
 
 	return editor;
 }
